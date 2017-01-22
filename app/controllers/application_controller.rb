1a1a9719e432238626be4dda49fcde3adb6ac60a
Ted <therealtedkennedy@gmail.com>
2017-01-22 17:45:26 -0500

    Added first name, last name and email to devise users.  Next up custom redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configured_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configured_permitted_parameters
+  	devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name,:last_name,:email,:password,:remember_me)}
+  	devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email,:password,:remember_me)} 
+  	devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name,:last_name,:current_artpromo,:email,:password,:remember_me)}
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122220926) do
+ActiveRecord::Schema.define(version: 20170122222105) do
 
   create_table "artpromoevents", force: :cascade do |t|
     t.integer  "artpromo_id"
@@ -71,6 +71,9 @@ ActiveRecord::Schema.define(version: 20170122220926) do
     t.integer  "failed_attempts",        default: 0,  null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "current_artpromo"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
